4dd398e40b13045d1cbfcd8aabe95009b4986b94
root <root@M.localdomain>
2020-07-22 22:10:17 +0200

    starting

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const compression = require('compression');
+const compression = require("compression");
+
+const cookieSession = require("cookie-session");
+/* const csurf = require("csurf");
+const { hash, compare } = require("./bc.js"); */
+
+//==============================middleware=====================================================================//
 
 app.use(compression());
+app.use(express.static(__dirname + "/public"));
 
-if (process.env.NODE_ENV != 'production') {
+//======cookie session middleware=================================//
+
+app.use(
+    cookieSession({
+        secret: `Ì am always angry.`,
+        maxAge: 1000 * 60 * 60 * 24 * 14, //to set the cookies-how long we want cookie to last
+    })
+);
+
+/* const cookieSessionMiddleware = cookieSession({
+    secret: `I'm always angry.`,
+    maxAge: 1000 * 60 * 60 * 24 * 90,
+});
+
+app.use(cookieSessionMiddleware);
+io.use(function (socket, next) {
+    cookieSessionMiddleware(socket.request, socket.request.res, next);
+}); */
+//======cookie session middleware=================================//
+
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+
+app.use(express.json());
+
+if (process.env.NODE_ENV != "production") {
     app.use(
-        '/bundle.js',
-        require('http-proxy-middleware')({
-            target: 'http://localhost:8081/'
+        "/bundle.js",
+        require("http-proxy-middleware")({
+            target: "http://localhost:8081/",
         })
     );
 } else {
-    app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
+    app.use("/bundle.js", (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
 
-app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+//==============================middleware=====================================================================//
+
+app.get("*", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(8080, function() {
+app.listen(8080, function () {
     console.log("I'm listening.");
 });
